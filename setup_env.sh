877baa140726ad758cca8da9d9676f3633f5d563
Fabiano Martins <fabiano.paula.martins@gmail.com>
2020-04-15 22:28:59 -0300

    Create a Setup script

---
+++ setup_env.sh
@@ -0,0 +1,16 @@
+
+# Adding softwares
+
+sudo apt-get install vim -y
+
+
+# Setup Vim Plugins
+
+rm -rf   ~/.vim
+mkdir -p ~/.vim/bundle
+
+cd ~/.vim
+
+git clone https://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
+
+
