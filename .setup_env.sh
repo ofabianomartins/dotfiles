d2a13943c0dc258cdd4437ae9dd9780546800129
Fabiano Martins <fabiano.paula.martins@gmail.com>
2020-04-15 23:24:24 -0300

    Install asdf in setup

--- .setup_env.sh
+++ .setup_env.sh
@@ -27,9 +27,39 @@ echo "alias config='/usr/bin/git --git-dir=/home/fabiano/.cfg/ --work-tree=/home
 # Generate SSH keys 
 
 # Personal SSH keys
-
 ssh-keygen -b 4096 -f "$HOME/.ssh/$(hostname)-key" -C "fabiano.paula.martins@gmail.com"
-
 # JOB SSH keys 
-
 ssh-keygen -b 4096 -f "$HOME/.ssh/$(hostname)-job-key" -C "fabiano@investtools.com.br"
+
+# Installing Asdf Language Version Manager
+
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+cd ~/.asdf
+git checkout "$(git describe --abbrev=0 --tags)"
+
+echo -e "\n. $HOME/.asdf/asdf.sh" >> ~/.zshrc
+
+# append completions to fpath
+echo -e "\nfpath=(${ASDF_DIR}/completions $fpath)" >> ~/.zshrc
+# initialise completions with ZSH's compinit
+echo -e "\nautoload -Uz compinit && compinit" >> ~/.zshrc
+
+sudo apt install \
+  automake autoconf libreadline-dev \
+  libncurses-dev libssl-dev libyaml-dev \
+  libxslt-dev libffi-dev libtool unixodbc-dev \
+  unzip
+
+# Install Asdf plugins 
+
+#Ruby
+asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
+
+#NodeJs
+asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+
+#Python
+asdf plugin-add python
+
+
