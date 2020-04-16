276fcb7706397b3be54090d0b25c99cb0fc0c20f
Fabiano Martins <fabiano.paula.martins@gmail.com>
2020-04-15 22:56:44 -0300

    Setting zsh as default shell

--- setup_env.sh
+++ setup_env.sh
@@ -3,6 +3,10 @@
 
 sudo apt-get install vim tmux zsh curl -y
 
+# Set Zsh as default 
+
+chsh -s $(which zsh)
+
 # Install Oh My Zsh
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
